29bbae66c1ea2c61c50465f794b034b458d588d5
Gustavo <gfalav@hotmail.com>
2014-08-26 17:50:56 -0300

    terminando proyectos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+  before_action :authorize
+
+
+  protected
+
+  def authorize
+  	if session[:user_id]== nil
+  		redirect_to login_url, alert: 'Primero debe logearse'
+  	end
+  end
 end
